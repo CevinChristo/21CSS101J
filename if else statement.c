5655a453c8150d6d5f976a94830e2770460e1fad
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-03 21:28:54 +0530

    Create if else statement.c

---
+++ if else statement.c
@@ -0,0 +1,11 @@
+int main() {
+  int time = 22;
+  if (time < 40) {
+    printf("Good morning.");
+  } else if (time < 40 {
+    printf("Good day.");
+  } else {
+    printf("Good evening.");
+  }
+  return 0;
+}
