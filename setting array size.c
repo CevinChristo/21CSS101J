3a90f13232d1a944fbe43b23956c082f48a5c851
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-03 22:35:56 +0530

    Create setting array size.c

---
+++ setting array size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  // Declare an array of four integers:
+  int myNumbers[4];
+
+  // Add elements to it
+  myNumbers[0] = 10;
+  myNumbers[1] = 20;
+  myNumbers[2] = 30;
+  myNumbers[3] = 40;
+
+  printf("%d\n", myNumbers[0]);
+ 
+  return 0;
+}
