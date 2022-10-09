78256436c1eb74e986894c6cff793795a2530684
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-09 13:39:06 +0530

    Create area of square.c

---
+++ area of square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ 
+int main() {
+   int side, area;
+ 
+   printf("\n Enter the Length of Side : ", side);
+   scanf("%d", &side);
+ 
+   area = side * side;
+   printf("\nArea of Square : %d", area);
+ 
+   return (0);
+}
