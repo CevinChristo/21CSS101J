d672265a52e1b14772b1d5936896ce7e287d0e79
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-12 11:22:14 +0530

    Update adding together different variables after giving them a value.c

--- adding together different variables after giving them a value.c
+++ adding together different variables after giving them a value.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  int sum1 = 10 + 500;        // 510 (10 + 500)
-  int sum2 = sum1 + 250;      // 760 (510 + 250)
-  int sum3 = sum2 + sum2;     // 1520 (760 + 760)
-  printf("%d\n", sum1);
+  int sum1 = 10 + 500;        
+  int sum2 = sum1 + 250;     
+  int sum3 = sum2 + sum2;     
   printf("%d\n", sum2);
   printf("%d\n", sum3);
   return 0;
