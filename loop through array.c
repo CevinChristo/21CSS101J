15f17bdb67311b433e154a78ecd0bee250c84668
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 11:52:22 +0530

    Update loop through array.c

--- loop through array.c
+++ loop through array.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main() {
+  printf("RA2211042010002\n\n");
   int myNumbers[] = {20, 40, 60, 80};
   int i;
-  
   for (i = 0; i < 4; i++) {
     printf("%d\n", myNumbers[i]);
   }
- 
   return 0;
 }
