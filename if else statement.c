10acf0a507d41a592b461ee0caad6805bcca0fa6
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 11:39:12 +0530

    Update if else statement.c

--- if else statement.c
+++ if else statement.c
@@ -1,10 +1,12 @@
 int main() {
+  printf("RA2211042010002\n\n");
   int time = 22;
   if (time < 40) {
     printf("Good morning.");
   } else if (time < 40 {
     printf("Good day.");
-  } else {
+  } else 
+  {
     printf("Good evening.");
   }
   return 0;
