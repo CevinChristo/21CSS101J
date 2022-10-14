417a80560d1d92fe01cbad1dc9c87d29c0a3ad98
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 11:20:51 +0530

    Update continue statement.c

--- continue statement.c
+++ continue statement.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main() {
+  printf("RA2211042010002\n\n");
   int i;
-  
   for (i = 0; i < 20; i++) {
     if (i == 5) {
       continue;
     }
     printf("%d\n", i);
   }   
-  
   return 0;
 }
