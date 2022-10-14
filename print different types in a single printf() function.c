0d54a3fb25d47dd1d3e4e121ba48656c8a3a3173
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 13:17:22 +0530

    Update print different types in a single printf() function.c

--- print different types in a single printf() function.c
+++ print different types in a single printf() function.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main() {
-
-int myNum = 5;
+  printf("RA2211042010002");
+ int myNum = 5;
 char myLetter = 'D';
 printf("My number is %d and my letter is %c", myNum, myLetter);
   return 0;
