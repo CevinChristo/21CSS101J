c685249d2565b0dbaa0fd7a169c4e5e6f60a3bc6
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 11:51:06 +0530

    Update logical or operator.c

--- logical or operator.c
+++ logical or operator.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
+ printf("RA2211042010002\n\n");
   int x = 6;
   int y = 3;
-  
-  // Returns 1 (true) because one of the conditions are true (6 is greater than 3, but 6 is not less than 4)
+   // shows 1 because 6 is greater than 3 but 6 is not less than 4.
   printf("%d", x > 3 || x < 4);
   return 0;
 }
