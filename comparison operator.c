071baa06ec93c1944f85716c39e4583a5fa10ff0
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 11:18:38 +0530

    Update comparison operator.c

--- comparison operator.c
+++ comparison operator.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
+  printf("RA2211042010002\n\n");
   int x = 18;
   int y = 19;
-  printf("%d", x > y); // returns 0 (false) because 18 is lesser than 19
+  printf("%d", x > y); // false as 18 is not greater thsn 19.
   return 0;
 }
