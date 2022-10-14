69276734b6665f1b258eef17942997d7661521da
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 11:15:31 +0530

    Update break out of a loop.c

--- break out of a loop.c
+++ break out of a loop.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 int main() {
-  int i;
-  
+ printf("RA2211042010002\n\n"); 
+  int i;  
   for (i = 0; i < 15; i++) {
     if (i == 8) {
       break;
     }
     printf("%d\n", i);
-  }
-   
+  } 
   return 0;
 }
