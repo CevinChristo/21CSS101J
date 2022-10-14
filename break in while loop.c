d40b522df162917670867932a1fb98cc998905c2
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 11:14:35 +0530

    Update break in while loop.c

--- break in while loop.c
+++ break in while loop.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 int main() {
-  int i = 0;
-  
+  printf("RA2211042010002\n\n");
+  int i = 0;  
   while (i < 15) {
     if (i == 8) {
       break;
     }
     printf("%d\n", i);
     i++;
-  } 
-  
+  }   
   return 0;
 }
