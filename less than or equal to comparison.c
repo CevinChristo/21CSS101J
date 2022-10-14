59eb6a4c1f78124739498d930bd663d9f8e3a0ef
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 11:46:57 +0530

    Update less than or equal to comparison.c

--- less than or equal to comparison.c
+++ less than or equal to comparison.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
+printf("RA2211042010002\n\n");  
   int x = 11;
   int y = 3;
-  printf("%d", x <= y); // returns 0 (false) because 11 is not less than 3
+  printf("%d", x <= y); //shows 0 as 11is not less than 3
   return 0;
 }
