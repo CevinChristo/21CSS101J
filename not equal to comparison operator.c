abbbe6fc49f10bf90dc196a7971ae79076add509
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 12:39:56 +0530

    Update not equal to comparison operator.c

--- not equal to comparison operator.c
+++ not equal to comparison operator.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main() {
+ printf("RA2211042010002\n\n");
   int x = 12;
   int y = 4;
-  printf("%d", x != y); // returns 1 (true) because 12 is not equal to 4
+  printf("%d", x != y); // shows 0 because 12 is not equal to 4
   return 0;
 }
