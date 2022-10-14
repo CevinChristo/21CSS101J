64362bd58f711c00618ef0e110e49af73ba9b267
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-10-14 12:54:52 +0530

    Update perimeter of parallelogram.c

--- perimeter of parallelogram.c
+++ perimeter of parallelogram.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 int main(){
+  printf("RA2211042010002\n\n"); 
     float num1, num2, perimeter;
-    
     printf("Enter the length of adjacent sides of the parallelogram: ");
     scanf("%f %f", &num1, &num2);
-    
     perimeter = 2 * (num1 + num2);
-    
     printf("Perimeter of the parallelogram is: %.2f", perimeter);
-    
     return 0;
 }
