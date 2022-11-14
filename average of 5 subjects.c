4f78f5866321c790f759af8c64cf49fbe9a3bb70
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-14 10:59:25 +0530

    Create average of 5 subjects.c

---
+++ average of 5 subjects.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main() {
+  printf("RA2211042010002");
+  int a, b, c, d, e;
+  printf("Enter marks for 5 subjects : ");
+  scanf("%d %d %d %d %d", &a, &b, &c, &d, &e);
+  float s=a+b+c+d+e,avg=s/5.0;
+  printf("total marks : %f\n", s);
+  printf("average marks : %f\n",avg);
+}
