1fd61223104c803164e358dc744f0976f5b0c994
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-19 18:13:01 +0530

    Create program to check eligibility to vote.c

---
+++ program to check eligibility to vote.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main( ) {
+printf("RA2211042010002\n\n");
+int age;
+printf("Enter your age: \n ");
+scanf(" %d ", &age);
+(age >= 18) ? printf(" You are eligible to Vote ") : printf(" You are not eligible to Vote ");
+return 0;
+} 
