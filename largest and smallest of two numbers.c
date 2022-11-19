ca4ffc9fa21783dfe86fc77c63d922b24d3cf44a
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-19 18:28:06 +0530

    Create largest and smallest of two numbers.c

---
+++ largest and smallest of two numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+void main() {
+  printf("RA2211042010002\n\n");
+  int x,y
+printf("Enter two numbers : ");
+  scanf("%d %d", &x, &y);
+  if(x>y) {
+    printf("The largest number : %d\nThe smallest number : %d\n", x, y);
+    else {
+      printf("The largest number : %d\nThe smallest number : %d\n", y, x);
+    } 
+  }
