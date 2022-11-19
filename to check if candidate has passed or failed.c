5d1454b7e087b93921e1e43acc9c0e42ba7a67f6
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-19 21:06:03 +0530

    Create to check if candidate has passed or failed.c

---
+++ to check if candidate has passed or failed.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+
+int main() {
+printf("RA2211042010002\n\n");
+int percentage;
+	printf("Enter percentage : ");
+	scanf("%d",&percentage);
+	if(percentage >= 40)
+		printf("\nResult is pass");
+	else
+		printf("\nResult is fail");
+	return 0;
+}
