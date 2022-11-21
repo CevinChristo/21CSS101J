99e5c4099885164a7436e542a5e4b2439947e796
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-21 23:50:27 +0530

    Create convert meter to foot.c

---
+++ convert meter to foot.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("RA2211042010002\n\n");
+  float foot, meter;
+  printf("Meter : ");
+  scanf("%f", &meter);
+  
+  printf("%f", foot = meter/3.281);
+}
