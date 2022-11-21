d46afb7ad671a703f034edf9cefaa06ee4e6c88c
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-21 23:51:46 +0530

    Create convert meter to millimeter.c

---
+++ convert meter to millimeter.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("RA2211042010002\n\n");
+  float milli, meter;
+  printf("Meter : ");
+  scanf("%f", &meter);
+  
+  printf("%f", milli = meter*1000);
+}
