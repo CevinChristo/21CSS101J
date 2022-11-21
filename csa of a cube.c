af7a94a0504e347ae0946fe9a370b08410bffe06
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-21 23:46:05 +0530

    Create csa of a cube.c

---
+++ csa of a cube.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main()
+{
+  printf("RA2211042010002\n\n");
+  int side, CSA;
+  printf("Side= ");
+  scanf("%d", &side);
+  
+  CSA = 4* side*side;
+  printf("CSA: %d", CSA);
+  
+}
