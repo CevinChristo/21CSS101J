6d89c750249b84e421df406116567df23e74a234
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-24 01:10:26 +0530

    Create program to check if number is greater than 100 or not.c

---
+++ program to check if number is greater than 100 or not.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+int number;
+printf("Enter an integer number\n\n");
+scanf("%d",&number);
+if(number<100) {
+printf("Number is smaller than 100\n); }
+else if (number=100); {
+printf("Number is 100); }
+else {
+printf("Number contains more than two digits and is greater than 100);
+}
+}
