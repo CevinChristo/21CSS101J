0c2bf5e548caeacf6f6fa8bdfd2d0a7f536130ce
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-24 01:28:35 +0530

    Create usage of getchar to print name.c

---
+++ usage of getchar to print name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+  printf(RA2211042010002\n\n);
+  char answer;
+  printf("Would you like to know my name?\n");
+  printf("Type Y for yes and N for no: ");
+  answer=getchar();
+  if(answer=='Y'||answer=='y')
+    printf("\n\nMy name is Cevin Christo\n);
+           else
+           printf("\n\nOk cool\n");
+           }
