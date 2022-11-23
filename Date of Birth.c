7f84654c1c1c12cdb6aca9c2f2289694ddbb749a
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-24 02:57:42 +0530

    Update Date of Birth.c

--- Date of Birth.c
+++ Date of Birth.c
@@ -2,8 +2,8 @@
  int main() {
    printf("RA2211042010002\n\n");
    int year,month,date;
-   printf("Enter DOB in yyyy.mm.dd : ");
+   printf("Enter DOB in yyyy.mm.dd : \n");
    scanf("%d.%d.%d",&year,&month,&date);
-   printf("DOB is : %d.%d.%d",year,month,date);
+   printf("DOB is : %d.%d.%d\n",year,month,date);
    return 0;
  }
