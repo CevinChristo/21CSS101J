de7d72832110e5bdf215589a2e86afea58f1e342
CevinChristo <109913508+CevinChristo@users.noreply.github.com>
2022-11-24 01:48:23 +0530

    Create program to evaluate y=x^n.c

---
+++ program to evaluate y=x^n.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+  printf("RA2211042010002\n\n");
+  int count, n;
+  float x, y;
+  printf("Enter the values of x and n : ");
+  scanf("%f %d", &x, &n);
+  y = 1.0;
+  count = 1;
+  while (count<=) {
+    y = y*x;
+    count++;
+  }
+ printf("\nx=%f; n= %d; x to the power n = %f\n",x,n,y);
+}
+    
+    
+      
